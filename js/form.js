8a02547d06d9c7368c8044724347f86f46ad6c2a
BishnujyotiDutta <bishnujyotidutta1@gmail.com>
2021-08-01 10:37:55 -0700

    41 project

---
+++ js/form.js
@@ -0,0 +1,53 @@
+class Form{
+    constructor(){
+       this.input = createInput("Name");
+       this.button = createButton('Play');
+       this.greeting = createElement('h2');
+       this.title = createElement('h2');
+       this.reset = createButton('Reset');
+    }
+    hide() {
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+        this.title.hide();
+    }
+    display() {
+        this.title.html("FRUIT CATCHER");
+        this.title.position(480, 50);
+        this.title.style('font-size', '70px');
+        this.title.style('color', 'white');
+        this.input.position(670,500);
+        this.input.style('width', '200px');
+        this.input.style('height', '20px');
+        this.input.style('background', 'lavender');
+        this.button.position(670,550);
+        this.button.style('width', '200px');
+        this.button.style('height', '40px');
+        this.button.style('background', 'lightpink');
+        this.reset.position(1160, 670);
+        this.reset.style('width', '100px');
+        this.reset.style('height', '30px');
+        this.reset.style('background', 'lightpink');
+
+        this.button.mousePressed(() => {
+            this.input.hide();
+            this.button.hide();
+            player.name = this.input.value();
+            playerCount += 1;
+            player.index = playerCount;
+            player.update();
+            player.updateCount(playerCount);
+            this.greeting.html("Hello " + player.name)
+            this.greeting.position(480,250);
+            this.greeting.style('color', 'black');
+            this.greeting.style('font-size', '100px');
+        });
+
+        this.reset.mousePressed(() => {
+            player.updateCount(0);
+            game.update(0);
+        });
+
+    }
+}
